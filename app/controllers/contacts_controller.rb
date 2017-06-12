6209f11581ce25eec32f3243795c04a0cefe7787
Kristof Cummingsgit config --global user.email kpac.ja@gmail.comc9 readme.mdgit config --global user.name Kristof <kpac.ja@gmail.com>
2017-06-12 02:28:59 +0000

    added comments and fixed email template

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,39 @@
 
 class ContactsController < ApplicationController
+  
+  #Get request  to /contact-us
+  #Show new contact form
   def new
     @contact = Contact.new
   end
   
+  # POST request to /contacts
   def create
+    #Mass assignmetn of form fields into contact object
     @contact = Contact.new(contact_params)
+    
+    #Save the contact object to DB
     if @contact.save
+      #Store form fields via parameters, into variables
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:comments]
+      # Plug variables into Contact Mailer email method and send email
       ContactMailer.contact_email(name, email, body).deliver
+      # Display success message in flash hash and redirect to new action
       flash[:success] = "Message Sent."
       redirect_to new_contact_path
     else
+      # If contact object doesnt save 
+      #show error messages in flash hash & redirect
       flash[:danger] = @contact.errors.full_messages.join(". ")
       redirect_to new_contact_path
     end
   end
   
   private
+  # To collect data from form we need to use strong params
+  # and whitelist form fields
   def contact_params
     params.require(:contact).permit(:name, :email, :comments)
   end
