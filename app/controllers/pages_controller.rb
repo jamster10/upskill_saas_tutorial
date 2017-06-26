303d2ee52a5dc01256dd28aa261c2e32ffd608de
Kristof Cummingsgit config --global user.email kpac.ja@gmail.comc9 readme.mdgit config --global user.name Kristof <kpac.ja@gmail.com>
2017-06-26 03:23:27 +0000

    Added plan variables and params to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
+  #get request for / which is home page
+  
   def home
+  @basic_plan = Plan.find(1)
+  @pro_plan = Plan.find(2)
   end
+  
+  #get request to about page /about
+  
   def about
   end
 end
