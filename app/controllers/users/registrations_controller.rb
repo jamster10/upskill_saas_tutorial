eac6cf03260f01292d734d492aac0e9fd9ef79c7
Kristof Cummingsgit config --global user.email kpac.ja@gmail.comc9 readme.mdgit config --global user.name Kristof <kpac.ja@gmail.com>
2017-08-20 21:09:22 +0000

    added code comments for stripe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
+    
+    #Extend default Devise gem behaviour so that users wil
+    #Pro accout (2) Save with a special subscription
+    #otherwise Devise signs them up as usual
     super do |resource|
       if params[:plan]
         resource.plan_id = params[:plan]
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   belongs_to :plan
          
   attr_accessor :stripe_card_token       
+  #If user pass validations (email etc) then call stripe and 
+  #tell stripe to set up subscription upon charging their card
+  # Stripe responds with customer data
+  #Store customer id as the customer token
   def save_withsubscription
     if valid?
      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
